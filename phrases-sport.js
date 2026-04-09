588066eac7db98dbbce85da0d736f5d1d50bab8d
Bassail <Bastien.sail@outlook.fr>
2026-04-09 08:13:47 +0000

    feat: création de HuddleGenerator

---
+++ phrases-sport.js
@@ -0,0 +1,8 @@
+// Phrases concernant l'aspect sportif du match
+const sportPhrases = [
+    "C'était un match accroché.",
+    "Votre zone nous a fait du mal dans un premier temps, puis on a su l’attaquer de plus en plus et on a réussi à la casser.",
+    "On a réussi à prendre les devants pendant la première partie du match, mais on a eu du mal à maintenir notre avance.",
+    "Vous avez réussi à prendre les devants pendant la première partie du match. On a eu du mal à réduire l'écart, la faute à une défense bien en place."
+];
+
